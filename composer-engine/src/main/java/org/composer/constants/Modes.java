27326b838ed017b19787e6e091407c05aeea9a2e
kaliv0 <kaloyan.ivanov88@gmail.com>
2023-04-09 11:42:16 +0300

    removed comments

--- composer-engine/src/main/java/org/composer/constants/Modes.java
+++ composer-engine/src/main/java/org/composer/constants/Modes.java
@@ -1,8 +1,8 @@
 package org.composer.constants;
 
 public class Modes {
-    public final static String KEY_SEPARATOR = " "; //TODO: use char instead?
-    public final static String F_KEY = "F"; //TODO: use char instead?
+    public final static String KEY_SEPARATOR = " ";
+    public final static String F_KEY = "F";
     public final static String MAJOR = "major";
     public final static String MINOR = "minor";
 }
